f40c9b4dd6d9ff317a6cd33c2350eb17d0d2b718
Sahiljeet singh kalsi <sahiljeetsinghkalsi@gmail.com>
2024-10-27 01:49:29 +0530

    Start

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { GlobalStyles } from "./Styles/global";
+
+function App() {
+  return (
+    <div className="App">
+      <GlobalStyles />
+      <div className="test">Testing</div>
+      hello world 
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Styles/global.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+  body {
+    background: black;
+    color:white;
+  }
+
+  .test{
+  font-size:10rem;
+  }
+`;
